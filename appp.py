97c810b26c85425b1b6fdbb04324acbda52b883b
SWAHILIHITS <96331266+SWAHILIHITS@users.noreply.github.com>
2023-08-05 09:17:23 +0300

    Update appp.py

--- appp.py
+++ appp.py
@@ -1,4 +1,7 @@
+import os,uvicorn
+from bot import Bot
+HOST = os.getenv("HOST", "0.0.0.0")
+PORT = os.getenv("PORT", 8080)   
+uvicorn.run(Bot(), host=HOST, port=PORT)
 
-def appr():
-  print("bot started")
-app=appr()
+
